92d6ffdfc8151ed984fac098c937c8189f5eb0fd
Adal <adalbertoavila1013@gmail.com>
2023-04-01 15:04:52 -0500

    basic structure for trip

---
+++ database/migrations/2023_04_01_195300_create_trips_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('distance', 3, 2);
+            $table->decimal('duration', 3, 2);
+            $table->unsignedBigInteger('driver_id');
+            $table->point('destination_location')->nullable();
+            $table->unsignedBigInteger('rider_id');
+            $table->point('origin_location')->nullable();
+            $table->enum('status', ['on trip', 'canceled', 'finished']);
+            $table->decimal('total_cost', 3, 2);
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('driver_id')->references('id')->on('drivers');
+            $table->foreign('rider_id')->references('id')->on('riders');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trips');
+    }
+};
