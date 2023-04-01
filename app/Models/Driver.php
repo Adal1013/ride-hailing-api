615ccb663dd68c160f49b38b5d2ba4937d18fa78
Adal <adalbertoavila1013@gmail.com>
2023-04-01 09:28:04 -0500

    basic structure for driver

---
+++ app/Models/Driver.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use MatanYadaev\EloquentSpatial\Objects\Point;
+use MatanYadaev\EloquentSpatial\Traits\HasSpatial;
+
+class Driver extends Model
+{
+    use HasFactory, HasSpatial;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'password',
+        'current_location',
+        'available'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password'
+    ];
+
+    /**
+     * The attributes that should be cast for serialization.
+     *
+     * @var string[]
+     */
+    protected $casts = [
+        'location' => Point::class
+    ];
+}
