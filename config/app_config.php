340b567a980dc0fddb6d9548b9ca3b310b4b5ad2
Adal <adalbertoavila1013@gmail.com>
2023-04-02 09:55:41 -0500

    add app_config file

---
+++ config/app_config.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+  'app_env' => env('APP_ENV'),
+  'test_token_card' => env('TEST_TOKEN_CARD'),
+  'wompi_public_key' => env('WOMPI_PUBLIC_KEY'),
+  'wompi_private_key' => env('WOMPI_PRIVATE_KEY'),
+  'wompi_url' => env('WOMPI_URL'),
+];
